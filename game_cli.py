6cea9eeaa93566f685f0f472ae5f94690f774b2c
oades <omaradesida@gmail.com>
2025-04-23 16:44:15 +0100

    add a cli module, more game logic

---
+++ game_cli.py
@@ -0,0 +1,11 @@
+def to_continue():
+    input("Press ENTER to continue.")
+
+def greeting():
+    print("Hello! Welcome to the game.")
+    to_continue()
+
+def select_move():
+    input("You're going to select tackle.")
+    player_choice = 'tackle'
+    return player_choice
--- game_logic.py
+++ game_logic.py
@@ -1,20 +1,42 @@
 import pokemon_game as pg
+import game_cli
 
+#setting health to be static for now
 player_health = 400
 cpu_health = 300
 
-
+#creating dict for player pokemon information
 player_pokemon = {
                 "total_health": player_health,
                 "current_health": player_health,
                 "move_power": pg.player_move_power,
               }
 
-
+#creating dict for cpu pokemon information
 cpu_pokemon = {
                 "total_health": cpu_health,
                 "current_health": cpu_health,
                 "move_power": pg.cpu_move_power,
               }
 
-#
+#While loop to evaluate the status of the battle
+while (player_pokemon["current_health"] > 0) and \
+      (cpu_pokemon["current_health"] > 0):
+
+    #player makes move
+    character_does_damage()
+
+    cpu_chooses_move()
+    character_does_damage()
+
+    #cpu makes move
+    #evaluate cpu action
+
+
+if player_pokemon["current_health"] <= 0:
+    print("Oh no you lost!")
+
+if cpu_pokemon["current_health"] <= 0:
+    print("Congrats you won!")
+
+exit()
