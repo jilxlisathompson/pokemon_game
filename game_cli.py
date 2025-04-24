f6239eb7803d5f7af828d394a56a8d48d8ab0ef2
oades <omaradesida@gmail.com>
2025-04-24 09:26:15 +0100

    improved readability of game_cli functions

--- game_cli.py
+++ game_cli.py
@@ -1,11 +1,13 @@
-def to_continue():
+def enter_to_continue():
     input("Press ENTER to continue.")
 
 def greeting():
     print("Hello! Welcome to the game.")
-    to_continue()
+    enter_to_continue()
 
 def select_move():
     input("You're going to select tackle.")
     player_choice = 'tackle'
     return player_choice
+
+
