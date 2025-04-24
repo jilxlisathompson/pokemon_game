7831906b71d59b8bb8ff696b7b244ae7adc615c1
oades <omaradesida@gmail.com>
2025-04-24 10:54:45 +0100

    select pokemon function fix

--- game_cli.py
+++ game_cli.py
@@ -1,13 +1,28 @@
+import pprint
+
 def enter_to_continue():
     input("Press ENTER to continue.")
+    return
 
 def greeting():
     print("Hello! Welcome to the game.")
     enter_to_continue()
+    return
 
 def select_move():
     input("You're going to select tackle.")
     player_choice = 'tackle'
     return player_choice
+
+def select_pokemon(pokemon_list = ('bulbasaur', 'charmander', 'squirtle')):
+    player_selection = '-'
+    while player_selection not in pokemon_list:
+        input("Please enter a Pokemon from the list")
+        pprint.pp(pokemon_list)
+
+
+    #is there a fetch function i can put here?
+
+    return player_selection
 
 
